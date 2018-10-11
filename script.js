3060f9a4817d728fb6bdde20d0e3494bad7c0ad8
SepehrSalim <sepehr.salim@gmail.com>
2018-10-11 16:06:12 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// All Variables
+/*
+    Just a good multiline comment
+*/
+var name = "Sepehr";
+var lastname = "Salim";
+console.log(name + ' ' + lastname);
+var age = prompt ('How old are you?');
+console.log(age);
+var married = prompt ('Are you married?');
+console.log('Married = ' + married);
+alert('Name = ' + name);
